8901b1284bb97890b7d79ccdf69ff7220657a7a1
Xavier Sanchez <xavier.espagne@gmail.com>
2026-04-08 13:51:13 +0200

    Atelier 1

--- Atelier 1/AtelierStock/AtelierStockTest/ProduitTest.cs
+++ Atelier 1/AtelierStock/AtelierStockTest/ProduitTest.cs
@@ -3,48 +3,78 @@ namespace AtelierStock
     [TestClass]
     public class ProduitTest
     {
+        private static Produit CreerProduit() => new("REF-001", "Stylo", 100.0m, 0.18m);
+
         [TestMethod]
-        public void Initialiser_ProduitQuelconque()
+        public void Initialiser_Produit_CopieLesDonneesEtDemarreEnRupture()
         {
-            // Arrange
-            var prixAchat = 100.0m;
-            var marge = 0.18m;
+            var produit = CreerProduit();
 
-            // Act
-            var p = new Produit("REF", "UnNom", prixAchat, marge);
-
-            // Assert
-            Assert.AreEqual("REF"  , p.Reference);
-            Assert.AreEqual("UnNom", p.Libelle);
-            Assert.AreEqual(100.0m , p.PrixAchat);
-            Assert.AreEqual(118.0m , p.PrixVente);
-            Assert.AreEqual(0, p.Stocks);
-            Assert.IsTrue(p.EstEnRupture);
-        }
-        [TestMethod]
-        public void Initialiser_ProduitMarge0()
-        {
-            // Arrange
-            // Act
-            var p = new Produit("REF", "UnNom", 100, 0);
-
-            // Assert
-            Assert.AreEqual("REF", p.Reference);
-            Assert.AreEqual("UnNom", p.Libelle);
-            Assert.AreEqual(100.0m, p.PrixAchat);
-            Assert.AreEqual(100.0m, p.PrixVente);
-            Assert.AreEqual(0, p.Stocks);
-            Assert.IsTrue(p.EstEnRupture);
+            Assert.AreEqual("REF-001", produit.Reference);
+            Assert.AreEqual("Stylo", produit.Libelle);
+            Assert.AreEqual(100.0m, produit.PrixAchat);
+            Assert.AreEqual(118.0m, produit.PrixVente);
+            Assert.AreEqual(0, produit.Stocks);
+            Assert.IsTrue(produit.EstEnRupture);
         }
 
         [TestMethod]
-        public void Initialiser_ProduitReferenceVide_LeveArgumentException()
+        public void Initialiser_Produit_MargeZero_DonnePrixVenteIdentiqueAuPrixAchat()
         {
-            Action act = () => new Produit("", "UnNom", 100, 0);
+            var produit = new Produit("REF-002", "Cahier", 100.0m, 0.0m);
 
-            Assert.Throws<ArgumentException>(act);
+            Assert.AreEqual(100.0m, produit.PrixAchat);
+            Assert.AreEqual(100.0m, produit.PrixVente);
         }
 
+        [TestMethod]
+        public void Rentrer_QuantitePositive_AugmenteLeStockEtSortDeRupture()
+        {
+            var produit = CreerProduit();
 
+            produit.Rentrer(5);
+
+            Assert.AreEqual(5, produit.Stocks);
+            Assert.IsFalse(produit.EstEnRupture);
+        }
+
+        [TestMethod]
+        public void Sortir_QuantiteInferieureOuEgaleAuStock_RetireExactementLaQuantiteDemandee()
+        {
+            var produit = CreerProduit();
+            produit.Rentrer(10);
+
+            var quantiteSortie = produit.Sortir(4);
+
+            Assert.AreEqual(4, quantiteSortie);
+            Assert.AreEqual(6, produit.Stocks);
+            Assert.IsFalse(produit.EstEnRupture);
+        }
+
+        [TestMethod]
+        public void Sortir_QuantiteEgaleAuStock_MetLeProduitEnRupture()
+        {
+            var produit = CreerProduit();
+            produit.Rentrer(3);
+
+            var quantiteSortie = produit.Sortir(3);
+
+            Assert.AreEqual(3, quantiteSortie);
+            Assert.AreEqual(0, produit.Stocks);
+            Assert.IsTrue(produit.EstEnRupture);
+        }
+
+        [TestMethod]
+        public void Sortir_QuantiteSuperieureAuStock_NeDoitPasGenererDeStockNegatifEtSignaleLaRupture()
+        {
+            var produit = CreerProduit();
+            produit.Rentrer(2);
+
+            var quantiteSortie = produit.Sortir(5);
+
+            Assert.AreEqual(2, quantiteSortie);
+            Assert.AreEqual(0, produit.Stocks);
+            Assert.IsTrue(produit.EstEnRupture);
+        }
     }
 }
